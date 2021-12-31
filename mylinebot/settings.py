71f3b44f749706eb31e305cfa91241e64e0b95ac
wilson20010327 <wilson.l20010327@gmail.com>
2022-01-01 03:01:28 +0800

    commit to heroku

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -18,7 +18,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-#STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -29,7 +28,7 @@ SECRET_KEY = 'django-insecure-)c=2e@ci-60615xh8=h55g!)+s2_*m=0n0x&zb!=naoz1=p9%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3343-36-236-170-188.ngrok.io']
+ALLOWED_HOSTS = ['lin-bot-class.herokuapp.com']
 
 
 # Application definition
